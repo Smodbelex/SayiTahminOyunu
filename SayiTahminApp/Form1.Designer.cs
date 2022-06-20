07fd72a9e41cbe23bb3a65641b2a3d75a43198e2
Smodbelex <emir.aslan.1999@gmail.com>
2022-06-21 01:59:15 +0300

    C# SayiTahminOyunu
    Bilgisayarın belirlediği sayıyı kullanıcıdan değer alarak tahmin ettirmek

---
+++ SayiTahminApp/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SayiTahminApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxTahmin = new System.Windows.Forms.TextBox();
+            this.labelSayi = new System.Windows.Forms.Label();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAdim = new System.Windows.Forms.Label();
+            this.lblTahminFarkı = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(72, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tahmininiz :";
+            // 
+            // txtBoxTahmin
+            // 
+            this.txtBoxTahmin.Location = new System.Drawing.Point(76, 305);
+            this.txtBoxTahmin.Multiline = true;
+            this.txtBoxTahmin.Name = "txtBoxTahmin";
+            this.txtBoxTahmin.Size = new System.Drawing.Size(218, 31);
+            this.txtBoxTahmin.TabIndex = 1;
+            this.txtBoxTahmin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxTahmin_KeyPress);
+            // 
+            // labelSayi
+            // 
+            this.labelSayi.AutoSize = true;
+            this.labelSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSayi.Location = new System.Drawing.Point(474, 202);
+            this.labelSayi.Name = "labelSayi";
+            this.labelSayi.Size = new System.Drawing.Size(16, 18);
+            this.labelSayi.TabIndex = 2;
+            this.labelSayi.Text = "0";
+            this.labelSayi.Visible = false;
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTahmin.Location = new System.Drawing.Point(76, 416);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(176, 56);
+            this.btnTahmin.TabIndex = 3;
+            this.btnTahmin.Text = "Tahmin Yap";
+            this.btnTahmin.UseVisualStyleBackColor = false;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(76, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(414, 161);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAdim
+            // 
+            this.lblAdim.AutoSize = true;
+            this.lblAdim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAdim.Location = new System.Drawing.Point(72, 208);
+            this.lblAdim.Name = "lblAdim";
+            this.lblAdim.Size = new System.Drawing.Size(122, 24);
+            this.lblAdim.TabIndex = 5;
+            this.lblAdim.Text = "Adım Sayısı : ";
+            // 
+            // lblTahminFarkı
+            // 
+            this.lblTahminFarkı.AutoSize = true;
+            this.lblTahminFarkı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTahminFarkı.Location = new System.Drawing.Point(257, 358);
+            this.lblTahminFarkı.Name = "lblTahminFarkı";
+            this.lblTahminFarkı.Size = new System.Drawing.Size(52, 24);
+            this.lblTahminFarkı.TabIndex = 6;
+            this.lblTahminFarkı.Text = "Fark:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(72, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tahmin Hakkında:";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.White;
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNewGame.Location = new System.Drawing.Point(508, 12);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(92, 161);
+            this.btnNewGame.TabIndex = 8;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(630, 525);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTahminFarkı);
+            this.Controls.Add(this.lblAdim);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnTahmin);
+            this.Controls.Add(this.labelSayi);
+            this.Controls.Add(this.txtBoxTahmin);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxTahmin;
+        private System.Windows.Forms.Label labelSayi;
+        private System.Windows.Forms.Button btnTahmin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAdim;
+        private System.Windows.Forms.Label lblTahminFarkı;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ SayiTahminApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTahminApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int adim_sayisi = 0;
+        public static string sayi1 = "";
+        public static string tahmin_Sayisi;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //BUTTON TİPİ
+            btnTahmin.TabStop = false;
+            btnTahmin.FlatStyle = FlatStyle.Flat;
+            btnTahmin.FlatAppearance.BorderSize = 0;
+            btnNewGame.TabStop = false;
+            btnNewGame.FlatStyle = FlatStyle.Flat;
+            btnNewGame.FlatAppearance.BorderSize = 0;
+            //
+            int sayac = 0;
+            Random RastgeleSayi = new Random();
+            //string sayi1 = "";
+            string bir, iki, uc, dort;
+            while (sayac==0)
+            {
+                sayi1 = RastgeleSayi.Next(1023, 9876).ToString();
+                bir = sayi1.Substring(0, 1);
+                iki = sayi1.Substring(1, 1);
+                uc = sayi1.Substring(2, 1);
+                dort = sayi1.Substring(3, 1);
+                if (bir != iki && bir != uc && bir != dort && iki != uc && iki != dort && uc != dort)
+                {
+                    labelSayi.Text = sayi1;
+                }
+                else  //Eğer rakamları farklı sayı üretemezse üretene kadar yeni başlat.
+                {
+                    Application.Restart();
+                }
+                sayac++;
+            }//while sonu
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            adim_sayisi++;
+            lblAdim.Text =("Adım Sayiniz: " + adim_sayisi.ToString());
+            int sayi = Convert.ToInt32(sayi1);
+            tahmin_Sayisi = txtBoxTahmin.Text; //tahmini String olarak aldım.
+            int tahmin_edilen_deger = Convert.ToInt32(tahmin_Sayisi);
+            if (tahmin_edilen_deger < sayi)
+            {
+                lblTahminFarkı.Text = "Tahmin değeriniz Sayı'dan küçük!";
+                //MessageBox.Show("Tahmin değeriniz Sayı'dan küçük!");
+                
+            }
+            else if (tahmin_edilen_deger > sayi)
+            {
+                lblTahminFarkı.Text = "Tahmin değeriniz Sayı'dan büyük!";
+                //MessageBox.Show("Tahmin ettiğiniz sayı değeri Sayı'dan büyük!");
+
+            }
+            else
+                lblTahminFarkı.Text = "Tebrikler Kazandınız!";
+            if(lblTahminFarkı.Text== "Tebrikler Kazandınız!")
+            {
+                txtBoxTahmin.Visible = false;
+                MessageBox.Show("Yeni oyun başlatılıyor!");
+                System.Threading.Thread.Sleep(2000);
+                Application.Restart();
+            }
+                
+               
+            //MessageBox.Show("Tebrikler Kazandınız!");
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yeni Oyun Başlatılıyor!");
+            Application.Restart();
+        }
+
+        private void txtBoxTahmin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }//form1 load sonu
+    }
+
